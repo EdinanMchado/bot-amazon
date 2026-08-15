39cc56161358f808c723b8ea79386de11e1b9d96
EdinanMchado <edinanmachado9@hotmail.com>
2026-08-15 18:06:26 -0300

    Update bot_amazon.py

--- bot_amazon.py
+++ bot_amazon.py
@@ -172,7 +172,7 @@ def buscar_ofertas_amazon(termo):
 def executar_monitoramento():
     for termo in TERMOS_BUSCA:
         buscar_ofertas_amazon(termo)
-        time.sleep(3)  # Pausa entre termos de busca para evitar bloqueios
+        time.sleep(7)  # Aumente de 3 para 7 segundos
 
 
 if __name__ == "__main__":
